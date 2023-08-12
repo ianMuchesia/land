6e8c61325b4696e8c16ad9f7081d047b3b255098
ianMuchesia <ianmuchesia@yahoo.com>
2023-08-12 16:58:44 +0300

    changes

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -3,7 +3,8 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className="px-4 pt-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 bg-[#fafafa]">
+    <div className='bg-[#fafafa]'>
+    <div className="px-4 pt-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 ">
     <div className="grid gap-10 row-gap-6 mb-8 sm:grid-cols-2 lg:grid-cols-4">
       <div className="sm:col-span-2">
         <Link
@@ -147,6 +148,7 @@ const Footer = () => {
         </li>
       </ul>
     </div>
+  </div>
   </div>
   )
 }
